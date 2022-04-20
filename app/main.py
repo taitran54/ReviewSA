3e72cb4d0a7b7f73082e42e1b64e3f9fc65cc7c6
Tran Nhan Tai <69810464+taitran54@users.noreply.github.com>
2022-04-20 12:30:39 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,20 +8,20 @@ from .modules.sentiment import predict_data
 from .routers import review
 app = FastAPI()
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:3000",
-]
+# origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost",
+#     "http://localhost:3000",
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app.include_router(review.router)
 
